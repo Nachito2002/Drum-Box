929aed3a4f5c63a03ce2fc9021c280f9598539a5
Ignacio Alejandro Torres Franceschi <torresignacio2002@gmail.com>
2020-11-09 20:18:09 -0300

    React environment created

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.[contenthash].js',
+		path: path.resolve(__dirname, 'build')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/i,
+				exclude: /node_modules/,
+				use: [
+					'babel-loader'
+				],
+				resolve: {
+					extensions: [
+						'.js',
+						'.jsx'
+					]
+				}
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				exclude: /node_modules/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		// new webpack.ProgressPlugin(),
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Drum Box',
+			template: './public/index.html'
+		})
+	],
+	mode: 'production'
+}
